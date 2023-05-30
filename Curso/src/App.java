8e694efd948dd6e7f3769670553ccd0631632be7
MN <107052006+Starangell@users.noreply.github.com>
2023-05-30 17:38:14 -0300

    primeiro commit, criação do repositorio

---
+++ Curso/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!!");
+    }
+}
+//Criação dessa pasta/repositorio: 30/05/2023; +/- ás 17h35
