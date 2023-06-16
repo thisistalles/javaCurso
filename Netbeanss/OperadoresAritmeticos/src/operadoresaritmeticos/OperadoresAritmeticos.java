a7f743c9de2f77bfc46904b629501e2150390c64
MN <107052006+Starangell@users.noreply.github.com>
2023-06-16 17:56:14 -0300

    Novos codigos,etc

---
+++ Netbeanss/OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Gtalles
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+    }
+    
+}
---
+++ Netbeanss/OperadoresAritmeticoss/src/operadoresaritmeticoss/OperadoresAritmeticoss.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticoss;
+
+/**
+ *
+ * @author Gtalles
+ */
+public class OperadoresAritmeticoss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       /* int n1 = 3;
+        int n2 = 5;
+        float m = (n1+n2)/2;
+        System.out.println("A Média é igual a " + m); */
+        
+        /*int numero = 10;
+        int valor = 4+ numero--;
+        System.out.println(valor);
+        System.out.println(numero);*/
+       
+       /*int x = 4;
+       x += 2; // x = x+2;
+        System.out.println(x);*/
+       
+       /*float v = 8.5f;
+       int ar = (int)Math.round(v);
+        System.out.println(ar);*/
+       
+       double ale = Math.random();
+       int n = (int) (1 + ale * (1000-1));
+        System.out.println(n);
+
+
+
+
+
+    }
+    
+}
