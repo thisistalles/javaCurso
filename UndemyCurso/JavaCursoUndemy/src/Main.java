7d7929fc046d47f03831df2ed4a6330f847678ef
MN <107052006+Starangell@users.noreply.github.com>
2023-08-21 20:44:23 -0300

    Testando

--- UndemyCurso/JavaCursoUndemy/src/Main.java
+++ UndemyCurso/JavaCursoUndemy/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Word!");
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Digite: ");
+        String digitee = input.next();
+        System.out.println(digitee);
+
+
 
         }
     }
