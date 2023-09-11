506b5b312566e07c8e23472bebe809b3f5aa732c
MN <107052006+Starangell@users.noreply.github.com>
2023-09-11 14:53:27 -0300

    coomit

--- UndemyCurso/Exercicios/Exercicio03/src/Main.java
+++ UndemyCurso/Exercicios/Exercicio03/src/Main.java
@@ -14,3 +14,4 @@ public class Main {
 Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
 de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
  */
+
