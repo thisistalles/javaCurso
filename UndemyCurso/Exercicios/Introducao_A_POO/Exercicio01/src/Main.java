06f135f8fc25dfd3911dc396e27665d773a4af17
MN <tgtallles362@gmail.com>
2024-01-13 17:58:39 -0300

    introducao a POO: codigo de aprendizado terminado, comecando exercicio 13/01/2024

---
+++ UndemyCurso/Exercicios/Introducao_A_POO/Exercicio01/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+/*Fazer um programa para ler os valores da largura e altura de um retângulo.
+* Em seguida, mostrar na tela o valor de sua área, perímetro e diagonal. Usar
+* uma classe como mostrado no projeto ao lado.
+*/
---
+++ UndemyCurso/Exercicios/Introducao_A_POO/Exercicio01/src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+    public double width;
+    public double height;
+
+    public double Area() {
+
+    }
+
+    public double Perimeter() {
+
+    }
+
+    public double Diagonal() {
+
+    }
+
+}
